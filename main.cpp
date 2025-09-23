8b98980f930e890643c62b2421f3e7507ad9cde7
livio <livio.personne@gmail.com>
2025-09-23 09:24:39 +0200

    media tests in main

--- main.cpp
+++ main.cpp
@@ -8,8 +8,14 @@
 
 int main(int argc, const char *argv[])
 {
-    Media media1 = Media("path/to/file/media1.mp3", "media1");
+    Media *media1 = new Media("path/to/file/media1.mp3", "media1");
+    Media *media2 = new Media("path/to/file/media2.mp4", "media2");
 
-    std::cout << media1 << std::endl;
+    std::cout << *media1 << std::endl;
+    std::cout << media1->getName() << std::endl;
+    media1->setName("newMedia1");
+    std::cout << *media1 << std::endl;
+    media2->setFilePath("new/path/to/file/media2.mp4");
+    std::cout << *media2 << std::endl;
     return 0;
 }
